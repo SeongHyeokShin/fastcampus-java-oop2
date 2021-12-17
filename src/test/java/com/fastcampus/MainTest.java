e2ab130b0d0073b88ee56f1a99122e3c4f26821c
SeongHyeokShin <sin4191@gmail.com>
2021-12-17 15:39:34 +0900

    Setup mainTest
    Maintest 셋업

---
+++ src/test/java/com/fastcampus/MainTest.java
@@ -0,0 +1,18 @@
+package com.fastcampus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+    @Test
+    void main (){
+        //Given
+        String [] args = {"3","2","4"};
+
+        //When
+        Main.main(args);
+
+        //Then
+    }
+}
